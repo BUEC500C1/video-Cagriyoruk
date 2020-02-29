b4e43abfba7152644be8f0fd5d319ada9c882084
Cagriyoruk <55101879+Cagriyoruk@users.noreply.github.com>
2020-02-28 21:05:08 -0500

    Update test_TweetToVideo.py

--- test_TweetToVideo.py
+++ test_TweetToVideo.py
@@ -3,3 +3,12 @@ from TweetToVideo import *
 
 def test_TTV():
   assert VideoSummary('eys',['lexfridman']) != None
+  assert VideoSummary('keys'['lexfridman', 'elonmusk', 'jomaoppa']) != None
+  with pytest.raises(FileNotFoundError):
+    assert VideoSummary('ky', ['fileNotFound'])
+
+def main():
+  test_TTV()
+
+if __name__ == '__main__':
+  main()
